590b5d03273b7546b75fc9614cf8a4e319a03498
nechai_codes <ne_chai@hotmail.com>
2025-11-03 21:41:42 +0200

    upd-hw

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,24 @@
-function getElementWidth(content, padding, border) {
-    return parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
+// Функція checkForSpam(message) приймає рядок (параметр message), перевіряє його на вміст 
+// заборонених слів spam і sale, і повертає результат перевірки. 
+// Слова в рядку параметра message можуть бути в довільному регістрі, наприклад SPAM або sAlE.
+
+// Доповни код функції таким чином, що:
+
+// Якщо знайдено заборонене слово (spam або sale), то функція повертає буль true
+// Якщо в рядку відсутні заборонені слова, функція повертає буль false
+
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. 
+// У консоль будуть виведені результати її роботи.
+
+function checkForSpam(message) {
+    const normalizedMessage = message.toLowerCase();
+        return (normalizedMessage.includes("spam")) || (normalizedMessage.includes("sale"));
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
